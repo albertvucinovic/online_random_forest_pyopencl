2bb353b18e53b8e1699cebe63bc332adce4b5ca8
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-10-27 12:08:05 +0200

    none

--- app/forex_io.py
+++ app/forex_io.py
@@ -7,4 +7,8 @@ def read_train_data_minute_rows(currency, since, upto):
   conn=psycopg2.connect("dbname=forex user=albert")
   cur=conn.cursor()
   cur.execute("select * from %s where date>='%s' and date<'%s'"%(currency, since, upto))
-  return cur.fetchall()
+  data =cur.fetchall()
+  cur.close()
+  conn.close()
+  return data
+
--- test/forex_io.py
+++ test/forex_io.py
@@ -10,8 +10,7 @@ class TestReadData(unittest.TestCase):
     print "Ovo je proba!"
     data=forex_io.read_train_data_minute_rows('eurusd_alpari', '2011.01.01', '2012.01.01')
     print "There are %d rows in data."%len(data)
+    assert(len(data)==368617)
     
-
-
 if __name__=='__main__':
   unittest.main()
