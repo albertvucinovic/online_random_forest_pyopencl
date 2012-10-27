a209267c4301e06dd1bd6c279b4c4c6594e7eb81
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-10-27 19:48:20 +0200

    none

--- app/worksheet.py
+++ app/worksheet.py
@@ -1,8 +1,11 @@
+import action_server.core.send_message_to_server as send_message
+
+
 def read_train_data(currency, since, upto, minutes_before, minutes_after):
   return {
     'algo':{
       'method':'read',
-      'exec':'import sys\nsys.path.append(\'..\')\nimport forex_io\n',
+      'exec':'from  ..app import forex_io',#import sys\nsys.path.append(\'..\')\nimport forex_io\n',
       'read method':'forex_io.read_train_data',
       'method args':{
         'currency':currency,
