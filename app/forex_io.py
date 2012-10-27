3dbd224fb2a62966fc2f0be11d05e98b06c32f15
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-10-27 11:46:41 +0200

    created a package with testing

--- app/forex_io.py
+++ app/forex_io.py
@@ -1,7 +1,10 @@
 import psycopg2
 
 def read_train_data(currency, since, upto, minutes_before, minutes_after):
+  data=read_train_data_minute_rows(currency, since, upto)
+
+def read_train_data_minute_rows(currency, since, upto):
   conn=psycopg2.connect("dbname=forex user=albert")
   cur=conn.cursor()
-  cur.execute("select * from %s where date>=%s and date<%s"%(currency, since, upto))
-  data=cur.fetchall()
+  cur.execute("select * from %s where date>='%s' and date<'%s'"%(currency, since, upto))
+  return cur.fetchall()
--- test/forex_io.py
+++ test/forex_io.py
@@ -1,3 +1,17 @@
-import 'test/unit'
+import unittest
+from ..app import forex_io
 
-class TestReadData<Test::Unit::TestCase
+class TestReadData(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def test_read_train_data(self):
+    print "Ovo je proba!"
+    data=forex_io.read_train_data_minute_rows('eurusd_alpari', '2011.01.01', '2012.01.01')
+    print "There are %d rows in data."%len(data)
+    
+
+
+if __name__=='__main__':
+  unittest.main()
