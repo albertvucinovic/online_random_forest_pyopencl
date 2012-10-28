78d485d6509d1db0d76b87f8f7b56dd6a3fc3286
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-10-28 11:38:37 +0100

    test features to predict

---
+++ test/features_to_predict.py
@@ -0,0 +1,17 @@
+import unittest
+from ..app import forex_io
+from ..app import features_to_predict
+
+class TestFeaturesToPredict(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_feature_names():
+    pass
+  
+  def test_transform():
+    pass
+
+
+if __name__=='__main__':
+  unittest.main()
