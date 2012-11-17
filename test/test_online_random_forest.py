8e5b080dde5b519c59a4a4679f4fc5ef2341f5ea
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-11-17 23:27:11 +0100

    test Gaussian Mixture Model

--- test/test_online_random_forest.py
+++ test/test_online_random_forest.py
@@ -18,6 +18,8 @@ class TestOnlineRandomForest(unittest.TestCase):
     gmm.fit(X)
     y_train_predict=gmm.predict(X)
     assert list(y_train_predict)==[1,1,0,0,0,1] or list(y_train_predict)==[0,0,1,1,1,0]
+    assert gmm.means_.mean()>1.45 and gmm.means_.mean()<1.55
+
 
 
 if __name__=='__main__':
