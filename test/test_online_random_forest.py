6677c1717d706ed3136b27765c5381e9699ce99c
Albert Vucinovic <albert.vucinovic@gmail.com>
2012-11-18 00:47:15 +0100

    test predict DecisionTree without samples

--- test/test_online_random_forest.py
+++ test/test_online_random_forest.py
@@ -20,6 +20,16 @@ class TestOnlineRandomForest(unittest.TestCase):
     assert list(y_train_predict)==[1,1,0,0,0,1] or list(y_train_predict)==[0,0,1,1,1,0]
     assert gmm.means_.mean()>1.45 and gmm.means_.mean()<1.55
 
+  def test_predict_without_samples(self):
+    d=orf.DecisionTree(
+      1, number_of_decision_functions=1, predict_without_samples={
+        'mean':5.0,
+        'variance':1.0,
+        'num_samples':1})
+    assert d.predict([1.])==5.
+    d.update([1.], 10.)
+    assert d.predict([1.])==7.5
+
 
 
 if __name__=='__main__':
